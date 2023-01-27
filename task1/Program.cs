b81e5ad2df6df8ab2ff8f081ed18f406d96bab04
Alen <Spookyshocky@ya.ru>
2023-01-27 21:06:17 +0100

    Выполнил дз для второго семинара

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿ //Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+
+Console.WriteLine("Напишите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int res = num % 100;
+int result = res / 10;
+
+Console.WriteLine($"Второе число цифры {num} будет {result}");
---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Напишите день недели цифрой: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num > 5 && num < 8)
+    Console.WriteLine("Выходной");
+if (num < 6 && num > 0)
+    Console.WriteLine("Будний");
+if (num > 7)
+    Console.WriteLine("Такого дня недели нет");
+
